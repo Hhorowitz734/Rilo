e4c0eb092fb6be14ffba1ef66369da47c8a59a8b
Hhorowitz734 <hhorowitz734@gmail.com>
2025-04-01 17:00:44 -0500

    changed crate to use rust naming conventions; added examples directory

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
+// src/main.rs
+
+use std::env;
+use std::fs;
+use std::process;
+
 mod token;
 mod lexer;
 
 use lexer::Lexer;
 use token::Token;
 
-
 fn main() {
-    let input = r#"
-        fn main() -> int {
-            let mut x = 42;
-            let y = x + 3;
-            return y;
-        }
-    "#;
+    
+    // Get args
+    let args: Vec<String> = env::args().collect();
 
-    let mut lexer = Lexer::new(input);
+    if args.len() != 2 {
+        eprintln!("Usage: rilo <file>");
+        process::exit(1);
+    }
+    
+    let filename = &args[1];
+
+    let source = fs::read_to_string(filename).unwrap_or_else(|err| {
+        eprintln!("Error reading file {}: {}", filename, err);
+        process::exit(1);
+    });
+    
+    // Loop over and parse source file
+    let mut lexer = Lexer::new(&source);
     loop {
    
         let tok = lexer.next_token();
